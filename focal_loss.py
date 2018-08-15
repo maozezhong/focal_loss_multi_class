6a62637e9ba7ac957c2ee29f4058433ddd3d7c9b
Spotty <spotty@Ubuntu16.04>
2018-08-15 23:41:42 +0800

    fc

--- focal_loss.py
+++ focal_loss.py
@@ -1,5 +1,5 @@
 # focal loss with multi label
-def focal_loss(gamma=2., alpha=.25, e=0.1, classes_num):
+def focal_loss(classes_num, gamma=2., alpha=.25, e=0.1):
     # classes_num contains sample number of each classes
     def focal_loss_fixed(target_tensor, prediction_tensor):
         '''
@@ -16,20 +16,14 @@ def focal_loss(gamma=2., alpha=.25, e=0.1, classes_num):
         FT = -1 * (one_minus_p ** gamma) * tf.log(tf.clip_by_value(prediction_tensor, 1e-8, 1.0))
 
         #2# get balanced weight alpha
-        classes_num = []    
         classes_weight = array_ops.zeros_like(prediction_tensor, dtype=prediction_tensor.dtype)
-        _, cls_num = prediction_tensor.shape
-        
-        sum_ = 0
+
         total_num = sum(classes_num)
-        for i in range(cls_num):
-            op = total_num / classes_num[i]
-            classes_weight[:,i] = op
-            sum_ += op
-        
-        # scale
-        for i in range(cls_num):
-            classes_weight[:,i] /= sum_
+        classes_w_t1 = [ total_num / ff for ff in classes_num ]
+        sum_ = sum(classes_w_t1)
+        classes_w_t2 = [ ff/sum_ for ff in classes_w_t1 ]   #scale
+        classes_w_tensor = tf.convert_to_tensor(classes_w_t2, dtype=prediction_tensor.dtype)
+        classes_weight += classes_w_tensor
 
         alpha = array_ops.where(target_tensor > zeros, classes_weight, zeros)
 
